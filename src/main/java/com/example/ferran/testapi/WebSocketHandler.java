464a41d39003cc97907ffb095c4c6bc7a54190ac
ferran <ferran.lopezpuig55@gmail.com>
2020-06-05 20:48:40 +0200

    init

---
+++ src/main/java/com/example/ferran/testapi/WebSocketHandler.java
@@ -0,0 +1,17 @@
+package com.example.ferran.testapi;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+import java.io.IOException;
+
+public class WebSocketHandler extends AbstractWebSocketHandler {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        String msg = String.valueOf(message.getPayload());
+        // Send back unique message depending on the id received from the client
+        System.out.println(msg);
+        session.sendMessage(new TextMessage("pringui"));
+    }
+}
